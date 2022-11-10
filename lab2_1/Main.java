f3099563605118f6e140f39315c2d6c439c729b4
MateuszSztefek <88203590+MateuszSztefek@users.noreply.github.com>
2022-11-10 19:22:17 +0100

    Delete lab2_1 directory

--- lab2_1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    public static void main(String[] args) {
-        Author author = new Author("John", "Smith");
-        Book book = new Book(author, 6, "EH324324HEDHE325GW", "Sci-fi", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel aliquet urna. Morbi sit amet dui quam. Donec erat erat, elementum eget quam nec, tincidunt mollis orci. Suspendisse fermentum mollis eros, ut iaculis nunc. Aliquam non malesuada enim. Morbi scelerisque eu erat vitae bibendum. Curabitur mattis malesuada nisi, ut viverra.");
-        
-        System.out.println(book.toString());
-    }
-}
